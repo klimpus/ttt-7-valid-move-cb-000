583682752369545153c07781b5a76ff145d066e4
Kristopher Limpus <krislimpus@gmail.com>
2018-04-23 22:37:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,9 +9,17 @@ end
 end
 
 def valid_move? (board, index)
-if board[index] == "X" || board[index] == "O" || index == 100
+if board[index] == "X" || board[index] == "O" ||
   return FALSE
-else
+else board[index] > 0 || board[index] < 8
   return TRUE
 end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
 end
